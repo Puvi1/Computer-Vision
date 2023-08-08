932523efa74ed198a93c084b643eb53bc53ff19b
Puviyarasu <108527641+Puvi1@users.noreply.github.com>
2023-08-08 14:00:12 +0530

    Update and rename 1.Image to Grayscale.py to Experiments/1.Image to Grayscale.py

--- Experiments/1.Image to Grayscale.py
+++ Experiments/1.Image to Grayscale.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 kernel = np.ones((5,5),np.uint8)
 print(kernel)
-path = "C:\\Users\\91984\\Pictures\\Screenshots\\Screenshot 2023-08-08 092113.png"
+path = r"C:\Users\svel5\Pictures\ROG_STRIX_product_wallpaper_1920x1200.jpg"
 img =cv2.imread(path)
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 cv2.imshow("GrayScale",imgGray)
